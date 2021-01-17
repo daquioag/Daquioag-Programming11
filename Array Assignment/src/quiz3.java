d5f618223050f40c213ad935438591a0763d2bc1
daquioag <70176847+daquioag@users.noreply.github.com>
2021-01-17 12:37:12 -0800

    Array Assignment Hand in

---
+++ Array Assignment/src/quiz3.java
@@ -0,0 +1,22 @@
+public class quiz3 {
+    //* public static int[] fillerUp(int[] nums) {
+    //   for (int i = 0; i < 250; i++) {
+    //     nums[i] = (int) Math.random()*3;
+    //}
+    //return nums;
+    // }
+
+    public static void main(String[] args) {
+        int nums = 0;
+        for (int i=0;i<5;i++){
+            nums--;
+            for(int j = 0; j<5;j++){
+                nums++;
+
+            }
+       
+        }
+        System.out.print("\n"+nums);
+    }
+
+}
